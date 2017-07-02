e49537a2eda9bb1525c8708ace5ede2e1f964a27
Derek Land <derek@ddmr.nl>
2017-07-02 14:34:51 +0200

    joystick om raspberry uit te zetten...

--- sensehat.py
+++ sensehat.py
@@ -10,22 +10,21 @@ import random
 import os
 
 
-global file
-global stream
-(file, filename) = tempfile.mkstemp(suffix='.csv', prefix='acc_', dir=tempfile._os.getcwd())
+global stream, shutdown_counter, measure
+(fileid, filename) = tempfile.mkstemp(suffix='.csv', prefix='acc_', dir=tempfile._os.getcwd()+'/data/')
 print('Output gaat naar: %s'%(filename))
-stream = open(file, 'a')
+stream = open(filename, 'a')
 
 start=50
 stop=255
 step=50
 
 sense=SenseHat()
-global measure
 measure = False
 rgb1=100
 rgb2=100
 rgb3=100
+shutdown_counter = 0
 
 def clamp(number, min_number=50, max_number=255):
     return min(max_number, max(min_number, value))
@@ -40,12 +39,28 @@ def randnumber(value):
         number =250
     return number
 
-
 def start():
-    sense.show_letter('S',text_colour=[255,0,0])
+    sense.show_letter('R',text_colour=[255,0,0])
 
 def stop():
     sense.set_pixels([[0,0,0]]*64)
+
+def shutdown():
+    global stream,shutdown_counter
+    if shutdown_counter == 0:
+        stop()
+        sense.show_letter('Q', text_colour=[0,255,0])
+        shutdown_counter = 1
+    elif shutdown_counter == 1:
+        sense.show_letter('Q', text_colour=[0,0,255])
+        shutdown_counter = 2
+    elif shutdown_counter == 2:
+        if not stream.closed:
+            stream.close()
+        stop()
+        sense.set_pixel(7,7,10,10,10)
+        os.system('sudo shutdown -h now')
+
 
 def run_measurement():
     data=sense.get_accelerometer_raw()
@@ -55,24 +70,28 @@ def run_measurement():
     stream.write('%s,%s,%s,%s\n'%(time.time(), data['x'], data['y'], data['z']))
 
 def startstop(event):
-    global measure, file, stream
+    global measure, filename, stream, shutdown_counter
     if event.action in ('pressed'):
+        print(event)
         if event.direction is 'down':
             measure = False
             time.sleep(0.1)
             stop()
             if not stream.closed:
                 stream.close()
+            shutdown_counter=0
         elif event.direction is 'up':
             if stream.closed:
                 stream = open(filename,'a')
             start()
             measure = True
+            shutdown_counter = 0
+        elif event.direction is 'middle':
+            print(shutdown_counter)
+            shutdown()
 
 measure = False
-sense.stick.direction_up = startstop
-sense.stick.direction_down = startstop
-
+sense.stick.direction_any = startstop
 while True:
     if measure:
         run_measurement()
